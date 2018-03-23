d9191bf085cedddc020cb036c470884c8ef5435d
AlloVince <allo.vince@gmail.com>
2018-03-23 11:04:48 +0800

    fix: fixed unit test

--- test/models/hexo_manager.js
+++ test/models/hexo_manager.js
@@ -2,14 +2,14 @@ import { test } from '../bootstrap';
 import HexoManager from '../../src/models/hexo_manager';
 
 test('Hexo parser', async (t) => {
-  t.throws(() => HexoManager.hexoParser('foo'), SyntaxError);
-  t.throws(() => HexoManager.hexoParser('---foo'), SyntaxError);
+  t.throws(() => HexoManager.hexoTextParser('foo'), SyntaxError);
+  t.throws(() => HexoManager.hexoTextParser('---foo'), SyntaxError);
 
-  t.deepEqual(HexoManager.hexoParser('---\nfoo: bar---\n\ncontent'), {
+  t.deepEqual(HexoManager.hexoTextParser('---\nfoo: bar---\n\ncontent'), {
     frontMatter: { foo: 'bar' },
     content: 'content'
   });
-  t.deepEqual(HexoManager.hexoParser('---\r\nfoo: bar---\r\n\r\ncontent'), {
+  t.deepEqual(HexoManager.hexoTextParser('---\r\nfoo: bar---\r\n\r\ncontent'), {
     frontMatter: { foo: 'bar' },
     content: 'content'
   });
